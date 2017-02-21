0ecd2b91e4124e3d22ff24ff0eecd00d7efa9935
Na Li <nli@albany.edu>
2017-02-20 20:42:07 -0500

    Add all the comment for the methods and some statements

--- Root.java
+++ Root.java
@@ -4,34 +4,65 @@ import java.util.Scanner;
  * Na Li
  * 2:45pm - 3:40pm, Monday
  */
-public class Root{
-  public static void main(String[] args){
+public class Root
+{
+  public static void main(String[] args)
+  {
+    //declare the type of variable appropriate 
     double number;
     double guess;
     double tolerate;
     int iteration = 0;
+    
     Scanner keyboard = new Scanner(System.in);
+    //Ask the user to input the number
     System.out.print("You want to get square root of: ");
     number = keyboard.nextDouble();
-    if(number <=0){
+    
+    if(number <=0)
+    { 
+      //Print a error message
+      //and ask the user to input the number again 
       System.out.println("Your did not enter a vaild number, please innput again");
       System.out.print("You want to get square root of: ");
       number = keyboard.nextDouble();
     }
+    
+    //Ask the user the tolerate
     System.out.print("How many error you will tolerate in the square root estimation?:");
     tolerate = keyboard.nextDouble();
+    //Pass the half of the number to the guess variable
     guess = number*0.5;
+    //Call the babylonian method that is the recrusive method
     babylonian(number,guess,iteration,tolerate);
   }
+  
+  /**
+   * The babylonian method that calculate the square root of the number and print it out.
+   * it is a recurisive method that call itself inside the method
+   * @param number1 The number of the user input
+   * @param lastGuess The guess of the square root
+   * @param iteration How many times the recrusive method was called by itself.
+   * @param tolerate How many error the user tolerate 
+   */
   public static void babylonian(double number1,double lastGuess,int iteration,double tolerate){
+    
+    //Declare the variable number2 and give the initial value 0.0
     double number2 =0.0;
+    
+    //The if statement have the base case of the method
+    //It prints out the message of the square root and the iteration of the recrusive method was called
     if(Math.abs(number1- lastGuess*lastGuess)<tolerate){
         System.out.println(lastGuess+" is the square root of "+number1);
         System.out.println("The recrusive method was called " +iteration+" times");
-      }
+    }
+    //Else statement that call the babylonian method 
     else{
+      //Increment the iteration
       iteration++;
+      //Pass the value to number2
       number2 = lastGuess+ (number1/lastGuess);
+      //Call the babylonian method itself
       babylonian(number1,0.5* number2,iteration,tolerate);
   }
 }
