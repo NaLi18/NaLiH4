c0fbc7b09281cd94547eaab4fedae67982265d68
Na Li <nli@albany.edu>
2017-02-20 19:59:41 -0500

    Creat a Scanner Object and ask the user to input the number which they want to get square root of.

--- Root.java
+++ Root.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
 public class Root{
   public static void main(String[] args){
     double number;
     double guess;
     double tolerate;
     int iteration = 0;
+    Scanner keyboard = new Scanner(System.in);
+    System.out.print("You want to get square root of: ");
+    number = keyboard.nextDouble();
   }
   public static void babylonian(double number1,double lastGuess,int iteration,double tolerate){
   }
