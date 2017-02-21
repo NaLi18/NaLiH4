895e18d78be060de695fba3645e97c47a1f15965
Na Li <nli@albany.edu>
2017-02-20 19:56:41 -0500

    declare all the fields of the Root class

--- Root.java
+++ Root.java
@@ -1,5 +1,9 @@
 public class Root{
   public static void main(String[] args){
+    double number;
+    double guess;
+    double tolerate;
+    int iteration = 0;
   }
   public static void babylonian(double number1,double lastGuess,int iteration,double tolerate){
   }
